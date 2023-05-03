b69a03dba66d62db3b67bf13c4820d5decaf7917
o273 <132451052+o273@users.noreply.github.com>
2023-05-03 12:40:08 +0200

    omar

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+let variable2 = 0
+let variable1 = 0
+input.onButtonPressed(Button.A, function () {
+    variable2 = 2
+    basic.showNumber(variable2)
+    variable1 = randint(0, 10)
+    basic.showNumber(variable1)
+    basic.showNumber(variable1 * variable2)
+})
+input.onButtonPressed(Button.B, function () {
+    variable1 = 3
+    basic.showNumber(variable2)
+    variable1 = randint(0, 10)
+    basic.showNumber(variable1)
+    basic.showNumber(variable1 + variable2)
 })
